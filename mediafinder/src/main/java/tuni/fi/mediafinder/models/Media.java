033e43286136366abcf7503212e2b5bb1baeb65b
knsach <knsach@avd-tau-s-01.ad.tuni.fi>
2023-10-07 00:43:28 +0300

    added media interface and utility class

---
+++ mediafinder/src/main/java/tuni/fi/mediafinder/models/Media.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package tuni.fi.mediafinder.models;
+
+import tuni.fi.mediafinder.utility.Utility;
+
+/**
+ *
+ * @author knsach
+ */
+public interface Media {
+    String getTitle();
+    String getReleaseDate();
+    Double getRating();
+    Utility.MediaType getMediaType();
+}
+
---
+++ mediafinder/src/main/java/tuni/fi/mediafinder/utility/Utility.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tuni.fi.mediafinder.utility;
+
+/**
+ *
+ * @author knsach
+ */
+public class Utility {
+    public static enum MediaType {
+        BOOK,
+        MOVIE
+    }
+}
+
